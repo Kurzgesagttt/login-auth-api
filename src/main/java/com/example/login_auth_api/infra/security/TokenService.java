e5cd017663ccb5d488b8d89d20711bda03344e98
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-02-25 15:01:02 -0300

    Initial commit

---
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,24 @@
+package com.example.login_auth_api.infra.security;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.login_auth_api.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+    public String generateToken(User user){
+
+        @Value("${api.security.token.secret}")
+        private String secret;
+
+        try{
+            Algorithm algorighm = Algorithm.HMAC256();
+
+        }catch (JWTCreationException e){
+            throw new RuntimeException("Error while authenticating");
+        }
+
+    }
+}
